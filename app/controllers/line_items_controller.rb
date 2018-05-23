4cfc95fd97af726078b87bf6d06bacbeb0cfc0c2
kowitkarunas <codykarunas@gmail.com>
2018-05-23 15:08:14 -0400

    Added some padding to the top carts show page.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -39,8 +39,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to items_path, notice: 'Item added to cart' }
-        # format.html { redirect_to @line_item.cart, notice: 'Item added to cart.' }
+        # format.html { redirect_to items_path, notice: 'Item added to cart' }
+        format.html { redirect_to @line_item.cart, notice: 'Item added to cart.' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
