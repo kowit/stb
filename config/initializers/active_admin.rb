cf88fce68ef1871b94021329ac2cd0ca87ee1312
kowitkarunas <codykarunas@gmail.com>
2018-05-25 16:46:35 -0400

    Added strong params to active_admin initializer

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,10 +1,15 @@
 ActiveAdmin.setup do |config|
+  # Strong parameters
+  config.before_filter do
+    params.permit!
+  end
+
   # == Site Title
   #
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Stb"
+  config.site_title = "Spill the Beans"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
